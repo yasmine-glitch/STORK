8043f6cb11f05eb86e745a72b704fdd85607d9b2
Caroline Gognet <carolinegognet@gmail.com>
2021-11-23 14:08:36 +0100

    show activities + cloudinary

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,12 @@
 class ActivitiesController < ApplicationController
 
   def index
+    @activity = Activity.all
+  end
 
+  def show
+    @activity = activity.find(params[:id])
+    @user = current_user
+    @booking = Booking.new
   end
 end
