d6af5e1213fb4467c719a5c2f1c33ae2fbf28f23
Yasmine BERRADA <yasmineberrada91@gmail.com>
2021-11-23 15:28:30 +0100

    created stork's seed V1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_150404) do
+ActiveRecord::Schema.define(version: 2021_11_23_115929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,9 @@ ActiveRecord::Schema.define(version: 2021_11_22_150404) do
     t.string "job_title"
     t.string "industry"
     t.text "bio"
+    t.string "country"
+    t.integer "age"
+    t.integer "years_exp"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
