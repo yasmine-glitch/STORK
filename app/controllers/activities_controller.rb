6a9420a7d48950f273b571a532116a60c8535ddb
Caroline Gognet <carolinegognet@gmail.com>
2021-11-23 15:15:57 +0100

    cloudinary view activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,6 +7,26 @@ class ActivitiesController < ApplicationController
   def show
     @activity = activity.find(params[:id])
     @user = current_user
+    @booking.user = @user
+    @users = User.all
+    @users.activity = @users
     @booking = Booking.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.owner = current_user
+    if @activity.save!
+      redirect_to activity_path(@activity)
+    else
+      render :new
+    end
+
+    end
+  end
+
+  private
+  def activity_params
+    params.require(:activity).permit(:name, :place, :address, :start_date, :end_date, :capacity_max, :is_full, :owner_id, :photo)
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :users, through: :bookings
+  has_one_attached :photo
 
   validates :name, :place, :start_date, :end_date, :capacity_max, presence: true
 end
