5f1599edd85937b69e2f16bbd567eca06369c6ea
Yasmine BERRADA <yasmineberrada91@gmail.com>
2021-11-30 18:49:32 +0100

    ça fonctionne bien le auto submit

--- app/javascript/controllers/geocoder_controller.js
+++ app/javascript/controllers/geocoder_controller.js
@@ -4,18 +4,23 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 export default class extends Controller {
   static values = { apiKey: String }
 
-  static targets = ["address"]
+  static targets = ["address", "autocomplete"]
 
   connect() {
+    console.log(this.addressTarget.value);
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
       types: "country,place,postcode,locality,address",
-      placeholder: 'Enter your location...',
+      placeholder: this.addressTarget.value == "" ? "Search for a city" : this.addressTarget.value,
+      clearOnBlur:false,
+      clearAndBlurOnEsc:false,
+      limit: 4,
       language: "en",
     });
-    this.geocoder.addTo(this.element);
+    this.geocoder.addTo(this.autocompleteTarget);
     this.geocoder.on("result", event => {
       this._setInputValue(event)
+      this.element.submit()
     });
     this.geocoder.on("clear", () => this._clearInputValue());
   }
