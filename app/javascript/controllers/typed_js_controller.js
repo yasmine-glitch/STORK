86a79c8d73dc35d20e9cae8b80c2ca5109b6d1ea
Caroline Gognet <carolinegognet@gmail.com>
2021-11-30 12:02:18 +0100

    starts home

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,7 +5,7 @@ import Typed from "typed.js";
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Develop your Network here, there, wherever you work!"],
+      strings: ["here.", "there.", "wherever you go!"],
       typeSpeed: 50,
       loop: true
     });
